5888bd0bd950e7d44aced8edf124f48f68305a4f
Tuan Nguyen <tuan@atndev.com>
2025-12-09 18:47:14 -0600

    Add files via upload

---
+++ www/geckoview-engine.js
@@ -0,0 +1,11 @@
+var GeckoViewEngine = {
+  getInfo: function () {
+    return Promise.resolve({
+      engine: 'geckoview',
+      provider: 'Mozilla Gecko',
+      embedded: true
+    });
+  }
+};
+
+module.exports = GeckoViewEngine;
